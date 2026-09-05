e3a478457ff6709d9bafa87bc3983326ebb73db7
nkumar71 <nkumar71@partner.jaguarlandrover.com>
2026-09-05 18:40:30 +0100

    Backend URL configured

--- adminpanel/src/pages/AdminLogin/AdminLogin.jsx
+++ adminpanel/src/pages/AdminLogin/AdminLogin.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import "./AdminLogin.css";
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL;
+const API_BASE = process.env.REACT_APP_URL;
 
 function AdminLogin() {
     const [data, setData] = useState({ email: "", password: "" });
--- foodies/src/pages/Profile/Profile.jsx
+++ foodies/src/pages/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import { StoreContext } from "../../context/StoreContext";
 import "./Profile.css";
 
 const API_BASE =
-  process.env.REACT_APP_API_BASE_URL;
+  process.env.REACT_APP_URL;
 
 function Profile() {
   const navigate = useNavigate();
